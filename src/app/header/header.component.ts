a5be03e12edb6bd6313d4aa0d7de405f322501be
R Hema <ramakrishnagarihema@gmail.com>
2023-07-08 20:59:23 +0530

    rectify the error in search functionality

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,FormsModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  searchText:any;
+  constructor(private router: Router) { }
+  onSearch(){
+    const queryParams = {searchString: this.searchText}
+    this.router.navigate(['/home'],{queryParams})
+  }
 
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from '../header/header.component';
 import { MenubarComponent } from '../menubar/menubar.component';
 import { CardComponent } from '../components/card/card.component';
 import { PaginationComponent } from '../pagination/pagination.component';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -22,13 +23,13 @@ import { PaginationComponent } from '../pagination/pagination.component';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   cards = data;
   pageResult = this.cards;
   currentPage = 1;
   page_size = 5;
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private route: ActivatedRoute) {
     console.log(data);
   }
 
@@ -37,14 +38,13 @@ export class HomeComponent {
   }
   onFilterSelect($event: any) {
     console.log($event);
-    if($event.category === "all"){
+    if ($event.category === 'all') {
       this.pageResult = this.cards;
-    } else{
+    } else {
       this.pageResult = this.cards.filter(
         ({ category }) => category === $event.category
       );
     }
-    
   }
 
   paginate(page_number: number) {
@@ -53,5 +53,17 @@ export class HomeComponent {
       (page_number - 1) * this.page_size,
       page_number * this.page_size
     );
+  }
+  ngOnInit(): void {
+    this.route.queryParams.subscribe((params) => {
+      const searchString =
+        this.route.snapshot.queryParamMap.get('searchString');
+      console.log({ searchString });
+      if (searchString) {
+        this.pageResult = this.cards.filter(
+          (card) => card.author.toLowerCase().indexOf(searchString.toLocaleLowerCase()) > -1
+        );
+      }
+    });
   }
 }
